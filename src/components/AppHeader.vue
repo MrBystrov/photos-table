7e51daa74cf2b71889c748905bf0640824b4f7db
Sergei <bodri4@yandex.ru>
2025-03-22 12:45:35 +0300

    Corrected input size

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
     <div class="p-4 flex items-center justify-between">
         <div class="search-group flex gap-2">
             <input v-model="query" type="text" placeholder="Введите ID альбомов через пробел"
-                class="p-2 border rounded" />
+                class="p-2 border rounded placeholder:text-[12px] w-[300px]" />
             <button @click="onSearch" :disabled="isLoading"
                 class="ml-2 p-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600 transition disabled:cursor-not-allowed disabled:bg-blue-300">
                 Поиск
